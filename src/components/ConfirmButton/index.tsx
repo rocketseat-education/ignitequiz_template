f0f6574c5767bd9d0ab60b0e81e6cec328d40d14
rodrigo <rodrigo.rgtic@gmail.com>
2023-02-10 08:32:20 -0300

    refactor: confirm button

--- src/components/ConfirmButton/index.tsx
+++ src/components/ConfirmButton/index.tsx
@@ -1,19 +1,20 @@
 import { Check } from 'phosphor-react-native';
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
 
-type Props = TouchableOpacityProps & {
-  onPress: () => void;
-}
-
-export function ConfirmButton({ ...rest }: Props) {
+export function ConfirmButton({ ...rest }: TouchableOpacityProps) {
   return (
     <TouchableOpacity
+      activeOpacity={0.7}
       style={styles.container}
       {...rest}
     >
+      <Text style={styles.title}>
+        Confirmar
+      </Text>
+
       <Check
         color={THEME.COLORS.WHITE}
         weight="bold"
--- src/components/ConfirmButton/styles.ts
+++ src/components/ConfirmButton/styles.ts
@@ -3,11 +3,20 @@ import { THEME } from '../../styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
-    width: 56,
+    flex: 1,
+    minHeight: 56,
+    maxHeight: 56,
     borderRadius: 6,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: THEME.COLORS.BRAND_MID,
     flexDirection: 'row',
+    overflow: 'hidden'
+  },
+  title: {
+    color: THEME.COLORS.WHITE,
+    fontFamily: 'Roboto_700Bold',
+    fontSize: 16,
+    marginRight: 7
   },
 });
